3cd3d8933de79109957fec14cb03f76a3d62cdef
Wasim GULISTAN <wgulista@e1r2p16.42.fr>
2016-04-26 15:54:01 +0200

    correction

--- includes/fdf.h
+++ includes/fdf.h
@@ -16,4 +16,28 @@
 # include "../minilibx/mlx.h"
 # include "../libft/includes/libft.h"
 
+# define UP 65
+# define DOWN 66
+# define RIGHT 67
+# define LEFT 68
+
+typedef struct		s_point
+{
+	int				x;
+	int				y;
+	int				color;
+	//t_point			*next;
+}					t_point;
+
+typedef struct		s_env
+{
+	void			*mlx;
+	void			*win;
+	char			*title;
+	int				width;
+	int				height;
+}					t_env;
+
+
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,45 @@
 #include "../includes/fdf.h"
 
-int		main()
+void		init_point(t_point *point)
 {
-	void	*mlx;
-	void	*win;
+	point->x = 0;
+	point->y = 0;
+	point->color = 0;
+	//point->next = NULL;
+}
 
-	mlx = mlx_init();
+void		init_env(t_env *env)
+{
+	env->mlx = mlx_init();
+	env->width = 500;
+	env->height = 500;
+	env->title = "Fil de fer";
+	env->win = mlx_new_window(env->mlx, env->width, env->height, env->title);
+	if (!env->win)
+	{
+		mlx_clear_window(env->mlx, env->win);
+		mlx_destroy_window(env->mlx, env->win);
+	}
+}
 
-	win = mlx_new_window(mlx, 500, 500, "Fil de fer");
-	mlx_loop(mlx);
+int			main()
+{
+	t_env		env;
+	t_point 	point;
+
+	init_env(&env);
+	init_point(&point);
+	point.x = 50;
+	while (point.x < 100)
+	{
+		point.y = 50;
+		while (point.y < 100)
+		{
+			point.color = 0x00FF00FF;
+			mlx_pixel_put(env.mlx, env.win, point.x, point.y, point.color);
+			point.y++;
+		}
+		point.x++;
+	}
+	mlx_loop(env.mlx);
 }
